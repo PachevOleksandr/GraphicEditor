b6d792c81b7a2ebc1a2b0e2397be4049ffb06fc5
Pachev Oleksandr <pachev7111@gmail.com>
2023-03-18 18:17:45 +0200

    Filled in area with controls

--- GraphicEditor/GraphicEditor/View/MainForm.Designer.cs
+++ GraphicEditor/GraphicEditor/View/MainForm.Designer.cs
@@ -3,12 +3,12 @@
     partial class MainForm
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,17 +23,162 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            drawingArea = new PictureBox();
+            toolStrip = new ToolStrip();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            openFileDialog = new OpenFileDialog();
+            saveFileDialog = new SaveFileDialog();
+            infoPanel = new TableLayoutPanel();
+            imageSizeInfoLabel = new Label();
+            mousePositionLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)drawingArea).BeginInit();
+            menuStrip1.SuspendLayout();
+            infoPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // drawingArea
+            // 
+            drawingArea.BackColor = SystemColors.ControlLightLight;
+            drawingArea.Location = new Point(0, 52);
+            drawingArea.Name = "drawingArea";
+            drawingArea.Size = new Size(800, 398);
+            drawingArea.TabIndex = 0;
+            drawingArea.TabStop = false;
+            drawingArea.SizeChanged += drawingArea_SizeChanged;
+            drawingArea.MouseMove += drawingArea_MouseMove;
+            // 
+            // toolStrip
+            // 
+            toolStrip.Location = new Point(0, 24);
+            toolStrip.Name = "toolStrip";
+            toolStrip.Size = new Size(800, 25);
+            toolStrip.TabIndex = 1;
+            toolStrip.Text = "toolStrip1";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, saveAsToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.ShortcutKeyDisplayString = "";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.O;
+            openToolStripMenuItem.Size = new Size(146, 22);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.S;
+            saveToolStripMenuItem.Size = new Size(146, 22);
+            saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(146, 22);
+            saveAsToolStripMenuItem.Text = "Save as";
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.FileName = "openFileDialog1";
+            // 
+            // infoPanel
+            // 
+            infoPanel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            infoPanel.ColumnCount = 3;
+            infoPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 120F));
+            infoPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 120F));
+            infoPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 570F));
+            infoPanel.Controls.Add(imageSizeInfoLabel, 0, 0);
+            infoPanel.Controls.Add(mousePositionLabel, 0, 0);
+            infoPanel.Location = new Point(0, 425);
+            infoPanel.Name = "infoPanel";
+            infoPanel.RowCount = 1;
+            infoPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            infoPanel.Size = new Size(800, 25);
+            infoPanel.TabIndex = 3;
+            // 
+            // imageSizeInfoLabel
+            // 
+            imageSizeInfoLabel.Dock = DockStyle.Left;
+            imageSizeInfoLabel.Location = new Point(123, 1);
+            imageSizeInfoLabel.Margin = new Padding(3, 1, 3, 1);
+            imageSizeInfoLabel.Name = "imageSizeInfoLabel";
+            imageSizeInfoLabel.RightToLeft = RightToLeft.No;
+            imageSizeInfoLabel.Size = new Size(114, 23);
+            imageSizeInfoLabel.TabIndex = 1;
+            imageSizeInfoLabel.Text = "Size: ";
+            imageSizeInfoLabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // mousePositionLabel
+            // 
+            mousePositionLabel.Dock = DockStyle.Left;
+            mousePositionLabel.Location = new Point(3, 1);
+            mousePositionLabel.Margin = new Padding(3, 1, 3, 1);
+            mousePositionLabel.Name = "mousePositionLabel";
+            mousePositionLabel.RightToLeft = RightToLeft.No;
+            mousePositionLabel.Size = new Size(114, 23);
+            mousePositionLabel.TabIndex = 0;
+            mousePositionLabel.Text = "Position: ";
+            mousePositionLabel.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(infoPanel);
+            Controls.Add(toolStrip);
+            Controls.Add(menuStrip1);
+            Controls.Add(drawingArea);
+            MainMenuStrip = menuStrip1;
+            Name = "MainForm";
+            Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)drawingArea).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            infoPanel.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox drawingArea;
+        private ToolStrip toolStrip;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private OpenFileDialog openFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private TableLayoutPanel infoPanel;
+        private Label mousePositionLabel;
+        private Label imageSizeInfoLabel;
     }
 }
--- GraphicEditor/GraphicEditor/View/MainForm.cs
+++ GraphicEditor/GraphicEditor/View/MainForm.cs
@@ -1,3 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace GraphicEditor.View
 {
     public partial class MainForm : Form
@@ -5,6 +15,26 @@ namespace GraphicEditor.View
         public MainForm()
         {
             InitializeComponent();
+            imageSizeInfoLabel.Text = $"Size: {drawingArea.Width} x {drawingArea.Height}";
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "Default image files|*.bmp;";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                drawingArea.Image = new Bitmap(openFileDialog.FileName);
+            }
+        }
+
+        private void drawingArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            mousePositionLabel.Text = $"Position: {e.X}, {e.Y}";
+        }
+
+        private void drawingArea_SizeChanged(object sender, EventArgs e)
+        {
+            imageSizeInfoLabel.Text = $"Size: {drawingArea.Width} x {drawingArea.Height}";
         }
     }
 }
